3e7d2f38631b172b15098b05b39ea6607547655e
muslumdogan01 <muslumdogan01@gmail.com>
2021-07-12 13:30:15 +0300

    feat: add menu bar

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-.fetch(url).then((res) res.data = muslum )
+let filmTürleri = document.querySelector('#filmTürleri');
+let showFilmList = document.querySelector('#showFilmList');
+let navbarMenu = document.querySelector('.navbar_menü');
+let closeButton = document.querySelector('#closeButton');
+
+let filmList = ["4K","Aile","Aksiyon","Animasyon","Belgesel","bilim-kurgu",
+"Dram","Fantastik","Gerilim","Gizem","Hint Filmleri" ,"Hint Filmleri","Kısa film","Komedi","Korku","Kült Filmleri"
+, "Macera", "Müzik","Romantik","Savaş","Stand Up","Stand Up", "Oscar Ödüllü Fimler","Suç",
+"Tarih","Tavsiye Fimler","Tv Film","Vahşi Batı"
+];
 
 
 
-
-muslum.forEach.
+filmList.forEach(function(films){
+    
+    document.querySelector('#filmTürleri').innerHTML += `<li class='liStyle'><a>${films}</a></li>`;
+    filmTürleri.childNodes
+})
+showFilmList.addEventListener('click', function(){
+    navbarMenu.classList.toggle('show');
+})
+// closeButton.addEventListener('click',function(){
+//     navbarMenu.classList.toggle('show');
+// })
