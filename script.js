71ba98f0b7f0b62c20487e799f89b349ddc6eb89
muslumdogan01 <muslumdogan01@gmail.com>
2021-07-13 19:31:43 +0300

    feat: add functionality for slider

--- script.js
+++ script.js
@@ -1,157 +1,213 @@
-let filmTürleri = document.querySelector('#filmTürleri');
-let showFilmList = document.querySelector('#showFilmList');
-let navbarMenu = document.querySelector('.navbar_menü');
-let closeButton = document.querySelector('#closeButton');
 
+
+let filmTürleri = document.querySelector("#filmTürleri");
+let showFilmList = document.querySelector("#showFilmList");
+let navbarMenu = document.querySelector(".navbar_menü");
 
 //  NavBar Alanı
 
-let filmList = ["4K","Aile","Aksiyon","Animasyon","Belgesel","bilim-kurgu",
-"Dram","Fantastik","Gerilim","Gizem","Hint Filmleri" ,"Hint Filmleri","Kısa film","Komedi","Korku","Kült Filmleri"
-, "Macera", "Müzik","Romantik","Savaş","Stand Up","Stand Up", "Oscar Ödüllü Fimler","Suç",
-"Tarih","Tavsiye Fimler","Tv Film","Vahşi Batı"
+let filmList = [
+  "4K",
+  "Aile",
+  "Aksiyon",
+  "Animasyon",
+  "Belgesel",
+  "bilim-kurgu",
+  "Dram",
+  "Fantastik",
+  "Gerilim",
+  "Gizem",
+  "Hint Filmleri",
+  "Hint Filmleri",
+  "Kısa film",
+  "Komedi",
+  "Korku",
+  "Kült Filmleri",
+  "Macera",
+  "Müzik",
+  "Romantik",
+  "Savaş",
+  "Stand Up",
+  "Stand Up",
+  "Oscar Ödüllü Fimler",
+  "Suç",
+  "Tarih",
+  "Tavsiye Fimler",
+  "Tv Film",
+  "Vahşi Batı",
 ];
 
-
-
-filmList.forEach(function(films){
-    
-    document.querySelector('#filmTürleri').innerHTML += `<li class='liStyle'><a>${films}</a></li>`;
-    filmTürleri.childNodes
-})
-showFilmList.addEventListener('click', function(){
-    navbarMenu.classList.toggle('show');
-})
+filmList.forEach(function (films) {
+  document.querySelector(
+    "#filmTürleri"
+  ).innerHTML += `<li class='liStyle'><a>${films}</a></li>`;
+  filmTürleri.childNodes;
+});
+showFilmList.addEventListener("click", function () {
+  navbarMenu.classList.toggle("show");
+});
 // closeButton.addEventListener('click',function(){
 //     navbarMenu.classList.toggle('show');
 // })
 
 //  NavBar Alanı Sonu
 
-
 // CONTENT SLİDER ALANI
 
-let models=[
-    {
-        name: 'Planet',
-        description:'David Attenborough: A life on Our',
-        imdb:'İMDB:6.4',
-        pixel:'1080p',
-        img:'/img/bmw.jpg'
+/* .fetch */
+/* res */
+/* 
+const myComponent = (movie) => {
+  return `<div class="content_slider_left">
+    <img class="content_slider_left_img" src="img/bmw.jpg" alt="Bmw" />
+    <div class="content_slider_left_body">
+      <span class="left_imdb">imdb ${movie.imdb}</span>
+      <span class="left_pixel">1080p</span>
+    </div>
+    <div class="content_slider_left_footer">
+      <i class="fas fa-chevron-left"></i>
+    </div>
+    <div class="content_slider_left_description">
+      <h1 class="content_slider_left_name">${movie.name}</h1>
+      <h2 class="content_slider_left_name2">${movie.desc}</h2>
+    </div>
+    </div>`;
+};
 
-    },
-    {
-        name: 'Enola Holmes',
-        description:'David Attenborough: A life on Our',
-        imdb:'İMDB:7.4',
-        pixel:'750p',
-        img:'/img/honda.jpg'
-    },
-    {
-        name: 'The Tomorrow War',
-        description:'Geleceğin Savaşı',
-        imdb:'3.4',
-        pixel:'1080p',
-        img:'img/mazda.jpg'
-    },
-    {
-        name: 'Enola Holmes',
-        description:'TheTomorrowWar',
-        imdb:'8.4',
-        pixel:'750p',
-        img:'img/skoda.jpg'
-    },
-    {
-        name: 'The Ice Road',
-        description:'The Ice Road',
-        imdb:'9.8',
-        pixel:'1080p',
-        img:'img/volvo.jpg'
-    },
-    {
-        name: 'Army of the Dead',
-        description:'Ölüler Ordusu',
-        imdb:'7.4',
-        pixel:'750p',
-        img:'img/bmw.jpg'
-    },
-    {
-        name: 'Nobody',
-        description:'Önemsiz Biri',
-        imdb:'2.4',
-        pixel:'1080p',
-        img:'img/mazda.jpg'
-    },
-]
+querySelector().innerHtml(myComponent(movie)); */
 
-let index=0;
-let index2=0;
-let slaytCount= models.length;
-let slaytCount2= models.length;
+let models = [
+  {
+    name: "Planet",
+    description: "David Attenborough: A life on Our",
+    imdb: "İMDB:6.4",
+    pixel: "1080p",
+    img: "/img/bmw.jpg",
+  },
+  {
+    name: "Enola Holmes",
+    description: "David Attenborough: A life on Our",
+    imdb: "İMDB:7.4",
+    pixel: "750p",
+    img: "/img/honda.jpg",
+  },
+  {
+    name: "The Tomorrow War",
+    description: "Geleceğin Savaşı",
+    imdb: "3.4",
+    pixel: "1080p",
+    img: "img/mazda.jpg",
+  },
+  {
+    name: "Sherlock Holmes",
+    description: "TheTomorrowWar",
+    imdb: "8.4",
+    pixel: "750p",
+    img: "img/skoda.jpg",
+  },
+  {
+    name: "The Ice Road",
+    description: "The Ice Road",
+    imdb: "9.8",
+    pixel: "1080p",
+    img: "img/volvo.jpg",
+  },
+  {
+    name: "Army of the Dead",
+    description: "Ölüler Ordusu",
+    imdb: "7.4",
+    pixel: "750p",
+    img: "img/bmw.jpg",
+  },
+  {
+    name: "Nobody",
+    description: "Önemsiz Biri",
+    imdb: "2.4",
+    pixel: "1080p",
+    img: "img/mazda.jpg",
+  },
+];
 
+/* Paramaters and states */
 
+const arrLength = models.length;
+let selectedIndex = 1;
+let startFrom = 0;
+let toEnd = 2;
+let arrLengthDivideTwo = Math.floor(arrLength / 2);
 
-// SLİDER LEFT
+/* Functions */
 
-showSlide(index)
+const manageSliceState = (index) => {
+  if (index == 0) {
+    return [startFrom, toEnd];
+  }
+  if (index > 0) {
+    startFrom += index * 2;
+    toEnd += index * 2;
+    return [startFrom, toEnd];
+  }
+};
 
-function showSlide(i){
+/*  Object slice method */
+const sliceMethodWithParams = (obj, from, to) => {
+  const sliceMeth = obj.slice(from, to);
+  return sliceMeth;
+};
 
-    index = i;
+/* Get sliced object */
 
-    if(i<0){
-        index = slaytCount -1;
-    }
-    if(i >= slaytCount){
-        index=0;
-    }
+const getObjectSliced = () => {
+  let sliceState = manageSliceState(selectedIndex);
+  let slicedObject = sliceMethodWithParams(
+    models,
+    sliceState[0],
+    sliceState[1]
+  );
+  return slicedObject;
+};
 
-    document.querySelector('.content_slider_left_name').textContent = models[index].name;
-    document.querySelector('.content_slider_left_name2').textContent=models[index].description;
-    document.querySelector('.content_slider_left_body .imdb').textContent=models[index].imdb;
-    document.querySelector('.content_slider_left_body .pixel').textContent=models[index].pixel;
-    document.querySelector('.content_slider_left_img').setAttribute('src',models[index].img);
-    
-}
+/* Button click method functions */
+const leftButton = (models) => {
+  if (selectedIndex == 0) {
+    return;
+  }
+  if (selectedIndex > 0) {
+    return (selectedIndex -= 1);
+  }
+};
 
+const rightButton = (models) => {
+  if (selectedIndex < arrLengthDivideTwo) {
+    return (selectedIndex += 1);
+  } else {
+    return;
+  }
+};
 
+/* Javascript DOM Methods */
 
+document.querySelector(".fa-chevron-left").addEventListener("click", () => {
+  leftButton(models);
+  document.querySelector(".left_imdb").textContent = getObjectSliced()[0].imdb;
+});
+document.querySelector(".fa-chevron-right").addEventListener("click", () => {
+  rightButton(models);
+  document.querySelector(".left_imdb").textContent = getObjectSliced()[0].imdb;
+});
 
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "3436f23d3d1c7bcaf2e7ec1a1611dc52";
 
-// document.querySelector('.card-link').setAttribute('href',models[index].link);
+let GetPopularMoviesEndpoint = `${BASE_URL}movie/popular?api_key=${API_KEY}`;
+let GetTopRatedMoviesEndpoint = `${BASE_URL}movie/top_rated?api_key=${API_KEY}`;
+let GetImageEndpoint = `https://image.tmdb.org/t/p/original/`;
+let client = document.querySelector("#client");
 
-document.querySelector('.fa-chevron-left').addEventListener('click', function(){
-    index--;
-    showSlide(index);
-    console.log(index);
-})
-
-//SLİDER RİGHT
-
-function showSlide(i){
-
-    index2 = i;
-
-    if(i<0){
-        index2 = slaytCount2 -1;
-    }
-    if(i >= slaytCount){
-        index2=0;
-    }
-    document.querySelector('.content_slider_right_name').textContent = models[index2].name;
-document.querySelector('.content_slider_right_name2').textContent=models[index2].description;
-document.querySelector('.content_slider_right_body .imdb').textContent=models[index2].imdb;
-document.querySelector('.content_slider_right_body .pixel').textContent=models[index2].pixel;
-document.querySelector('.content_slider_right_img').setAttribute('src',models[index2].img);
-
-}
-
-
-document.querySelector('.fa-chevron-right').addEventListener('click', function(){
-    index2++;
-    showSlide(index2);
-    console.log(index2);
-})
-
-
-// CONTENT SLİDER ALANI SONU
+/* For images  */
+/* 
+  fetch(GetTopRatedMoviesEndpoint)
+  .then((Response) => Response.json())
+  .then((json) => {
+    console.log("Top Rated", json);
+  }); */
